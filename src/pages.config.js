516db89a02148d29191cc21ce24eae2dce44ed80
BOB Ecosphère du Genevois <blackystark65@gmail.com>
2026-01-03 22:23:49 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,15 +1,11 @@
-import Game from './pages/Game';
 import Home from './pages/Home';
-import Learn from './pages/Learn';
 
 
 export const PAGES = {
-    "Game": Game,
     "Home": Home,
-    "Learn": Learn,
 }
 
 export const pagesConfig = {
-    mainPage: "Game",
+    mainPage: "Home",
     Pages: PAGES,
 };
