f0118f9b2e7bbcc8119c124225bca73fecb02c80
BOB Ecosphère du Rosey <blackystark65@gmail.com>
2026-03-16 17:31:33 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,9 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/BiomeTemperateForest" element={<LayoutWrapper currentPageName="BiomeTemperateForest"><BiomeTemperateForestPage /></LayoutWrapper>} />
+      <Route path="/BiomeWetlands" element={<LayoutWrapper currentPageName="BiomeWetlands"><BiomeWetlandsPage /></LayoutWrapper>} />
+      <Route path="/BiomeMountains" element={<LayoutWrapper currentPageName="BiomeMountains"><BiomeMountainsPage /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
